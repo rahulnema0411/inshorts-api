225a28e2edc567c244a4fd2f8993e7cb13a7b61b
Rahul Nema <43380701+rahulnema0411@users.noreply.github.com>
2020-11-09 21:17:41 +0530

    GET and POST are working

--- main.go
+++ main.go
@@ -9,9 +9,9 @@ import (
 	"net/http"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 type Article struct {
@@ -21,38 +21,76 @@ type Article struct {
 	Content  string `json:"content"`
 }
 
-var Articles []Article
-
 func homePage(response http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(response, "Welcome to the HomePage!")
 	fmt.Println("Endpoint Hit: homePage")
 }
 
 func articles(response http.ResponseWriter, request *http.Request) {
+
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	ctx, _ := context.WithTimeout(context.Background(), 2*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		panic(err)
+	}
+
 	fmt.Println("methods", request.Method)
 	if request.Method == "GET" {
-		json.NewEncoder(response).Encode(Articles)
+		var allArticles []Article
+		collection := client.Database("inshorts").Collection("news")
+		ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+		cursor, err := collection.Find(ctx, bson.M{})
+		if err != nil {
+			response.WriteHeader(http.StatusInternalServerError)
+			response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+			return
+		}
+		defer cursor.Close(ctx)
+		for cursor.Next(ctx) {
+			var article Article
+			cursor.Decode(&article)
+			allArticles = append(allArticles, article)
+		}
+		json.NewEncoder(response).Encode(allArticles)
 	} else if request.Method == "POST" {
+
+		var article Article
+		_ = json.NewDecoder(request.Body).Decode(&article)
+		collection := client.Database("inshorts").Collection("news")
+
+		result, _ := collection.InsertOne(ctx, article)
+		json.NewEncoder(response).Encode(result)
 		body, err := ioutil.ReadAll(request.Body)
 		if err != nil {
 			panic(err)
 		}
 		log.Println(string(body))
-		var article Article
-		json.Unmarshal([]byte(body), &article)
-		fmt.Println("Printing response")
-		fmt.Println(article.ID)
-		fmt.Println(article.Title)
-		fmt.Println(article.Subtitle)
-		fmt.Println(article.Content)
+
 	}
 }
 
 func search(response http.ResponseWriter, request *http.Request) {
 	fmt.Println("GET params were:", request.URL.Query())
 	query := request.URL.Query().Get("q")
-	fmt.Fprintf(response, "Welcome to the searchPage!")
+
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	ctx, _ := context.WithTimeout(context.Background(), 2*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	var article Article
+	collection := client.Database("inshorts").Collection("news")
 	fmt.Println(query)
+	err = collection.FindOne(ctx, Article{ID: query}).Decode(&article)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(article)
 }
 
 func getArticle(response http.ResponseWriter, request *http.Request) {
@@ -68,29 +106,6 @@ func handleRequests() {
 }
 
 func main() {
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
 
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	ctx, _ := context.WithTimeout(context.Background(), 2*time.Second)
-	err = client.Connect(ctx)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = client.Ping(ctx, readpref.Primary())
-
-	Articles = []Article{
-		Article{ID: "1", Title: "Hello 2", Subtitle: "Article Description", Content: "Article Content"},
-		Article{ID: "2", Title: "Hello 2", Subtitle: "Article Description", Content: "Article Content"},
-		Article{ID: "3", Title: "Hello 2", Subtitle: "Article Description", Content: "Article Content"},
-		Article{ID: "4", Title: "Hello 2", Subtitle: "Article Description", Content: "Article Content"},
-		Article{ID: "5", Title: "Hello 2", Subtitle: "Article Description", Content: "Article Content"},
-		Article{ID: "6", Title: "Hello 2", Subtitle: "Article Description", Content: "Article Content"},
-		Article{ID: "7", Title: "Hello 2", Subtitle: "Article Description", Content: "Article Content"},
-	}
 	handleRequests()
 }
